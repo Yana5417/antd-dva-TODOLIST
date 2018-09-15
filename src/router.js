b9f10a41584fee1e11f9861e7adc606857fddc7f
Yana5417 <1404578601@qq.com>
2018-09-15 13:47:10 +0800

    页面结构整理

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
-import Products from './routes/Products';
+import IndexPage from './routes/IndexPage/index';
+import Products from './routes/ProductsPage/index';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
-        <Route path="/products" exact component={Products} />
+        <Route path="/" exact component={Products} />
+        <Route path="/index" exact component={IndexPage} />
       </Switch>
     </Router>
   );
--- src/routes/ProductsPage/index.js
+++ src/routes/ProductsPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
-import ProductList from '../components/ProductList';
+import ProductList from '../../components/ProductList';
+import styles from './index.css';
 
 const Products = ({ dispatch, products }) => {
   function handleDelete(id) {
@@ -11,7 +12,7 @@ const Products = ({ dispatch, products }) => {
   }
 
   return (
-    <div>
+    <div className={styles.Products}>
       <h2>List of Products</h2>
       <ProductList products={products} onDelete={handleDelete}></ProductList>
     </div>
